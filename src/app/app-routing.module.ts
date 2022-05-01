d7353bb0e3dc9d6c8d7192435762dad23131520b
DiabateJS <diabatejs@yahoo.fr>
2022-05-01 14:42:48 +0200

    Début intégration page cv-tech

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AcademyComponent } from './academy/academy.component';
+import { ContactComponent } from './contact/contact.component';
+import { CvTechComponent } from './cv-tech/cv-tech.component';
+import { EntrepriseComponent } from './entreprise/entreprise.component';
+import { HomeComponent } from './home/home.component';
+import { ParticulierComponent } from './particulier/particulier.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'home', component: HomeComponent},
+  {path:'cv-tech', component: CvTechComponent},
+  {path:'a-propos', component: AboutUsComponent},
+  {path:'entreprise', component: EntrepriseComponent},
+  {path:'particulier', component: ParticulierComponent},
+  {path:'academy', component: AcademyComponent},
+  {path:'contact', component: ContactComponent},
+  {path:'', redirectTo: 'home', pathMatch: 'full'},
+  {path:'**', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { CvTechComponent } from './cv-tech/cv-tech.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { EntrepriseComponent } from './entreprise/entreprise.component';
+import { ParticulierComponent } from './particulier/particulier.component';
+import { AcademyComponent } from './academy/academy.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    CvTechComponent,
+    AboutUsComponent,
+    EntrepriseComponent,
+    ParticulierComponent,
+    AcademyComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
